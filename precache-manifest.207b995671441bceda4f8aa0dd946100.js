cf4b0ae88ea12aa78c8d5ee39e801e2cd55bec1f
Alejandra Casado <z.prende.a@gmail.com>
2022-05-18 16:22:41 -0300

    build prueba

---
+++ precache-manifest.207b995671441bceda4f8aa0dd946100.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bc1896a942a499cc847",
+    "url": "/static/css/main.241bd277.chunk.css"
+  },
+  {
+    "revision": "4bc1896a942a499cc847",
+    "url": "/static/js/main.4bc1896a.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "9f467190b4df4fe2318e",
+    "url": "/static/js/2.9f467190.chunk.js"
+  },
+  {
+    "revision": "ae42267ff6069bcb7427",
+    "url": "/static/js/runtime~main.ae42267f.js"
+  },
+  {
+    "revision": "1632dcdf432c1207391bfdc53955d269",
+    "url": "/index.html"
+  }
+];
